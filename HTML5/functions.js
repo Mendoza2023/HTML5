36e98f900e732e1b4a98906a75409f9f3914be64
Marcos Mendoza <126982893+Mendoza2023@users.noreply.github.com>
2023-04-24 09:35:12 -0600

    Add files via upload
    html5 login

---
+++ HTML5/functions.js
@@ -0,0 +1,5 @@
+function greeting() {
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    alert("Hello  " + name + ",   you have " + age + "  years old")
+}
